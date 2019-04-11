fc6c8d8176bafa521d54382cb115aea1057a1fed
arturofdezperez <artfep@uma.es>
2019-04-11 19:48:24 +0200

    Mejorada identificación de IP

--- networkInterfaces.py
+++ networkInterfaces.py
@@ -6,7 +6,7 @@
 #                                           #
 #############################################
 
-import psutil
+import psutil, socket
 
 addrs = psutil.net_if_addrs() #Para obtener las interfaces y sus direcciones MAC
 stats = psutil.net_if_stats() #Para saber si la interfaz esta activa
@@ -19,7 +19,7 @@ for i in addrs:
 	#Para cada elemento t de la tupla en el valor que corresponde a i
 	for t in addrs[i]:
 		#Si tiene asignada IP, guardo los datos de la (sub)red
-		if t.family == 2:
+		if t.family == socket.AF_INET:
 			ip = t.address
 			nm = t.netmask
 			bc = t.broadcast
@@ -28,7 +28,7 @@ for i in addrs:
 		if t.family == psutil.AF_LINK:
 			mac = t.address
 			#Muestro el nombre de la interfaz y su direccion MAC
-			print(i + ": " + str.upper(mac), end =" ")
+			print(f"{i}: {mac}", end =" ")
 
 			#Muestro si es local o globalmente administrada
 			#(penultimo bit del primer byte)
@@ -48,21 +48,21 @@ for i in addrs:
 			#Muestra (si tiene asingadas) la IP, la mascara de red
 			#y la direccion de broadcast
 			if ip != None:
-				print("- IP: " + ip)
-				print("- Netmask: " + str(nm))
-				print("- Broadcast: " + str(bc))
+				print(f"- IP: {ip}")
+				print(f"- Netmask: {nm}")
+				print(f"- Broadcast: {bc}")
 
 			#Muestro si la interfaz es full-duplex o half-duplex, o desconocido
-			if stats[i].duplex == 2:
+			if stats[i].duplex == psutil.NIC_DUPLEX_FULL:
 				print("- Duplex mode: FULL-DUPLEX")
-			elif stats[i].duplex == 1:
+			elif stats[i].duplex == psutil.NIC_DUPLEX_HALF:
 				print("- Duplex mode: HALF-DUPLEX")
 			else:
 				print("- Duplex mode: Unknown")
 			#Muestro la velocidad de la interfaz
-			print("- Speed (MB/s): " + str(stats[i].speed))
+			print(f"- Speed (MB/s): {stats[i].speed}")
 			#Muestro el MTU
-			print("- MTU: " + str(stats[i].mtu))
+			print(f"- MTU: {stats[i].mtu}")
 
 			print()
 			ip = None
